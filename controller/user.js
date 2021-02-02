62449a92b8f1e7166a6750b90ee86b9d6c74b4c3
Parker Bjur <bjur.parker45@gmail.com>
2021-02-02 14:36:25 -0800

    Add: login function

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,6 @@
+/* eslint-disable consistent-return */
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const UserModel = require('../model/userModel');
 
 exports.signup = (req, res, next) => {
@@ -27,5 +29,43 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
+  UserModel.findOne({ email: req.body.email }).then(
+    (user) => {
+      if (!user) {
+        return res.status(401).json({
+          error: new Error('User not found!'),
+        });
+      }
+      bcrypt.compare(req.body.password, user.password).then(
+        (valid) => {
+          if (!valid) {
+            return res.status(401).json({
+              error: new Error('Incorrect password!'),
+            });
+          }
+          const token = jwt.sign(
+            { userId: user._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' },
+          );
+          res.status(200).json({
+            userId: user._id,
+            token,
+          });
+        },
+      ).catch(
+        (error) => {
+          res.status(500).json({
+            error,
+          });
+        },
+      );
+    },
+  ).catch(
+    (error) => {
+      res.status(500).json({
+        error,
+      });
+    },
+  );
 };
--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,6 @@ const router = require('./routes');
 
 router.post('/api/signup', userControl.signup);
 
+router.post('/api/login', userControl.login);
+
 module.exports = router;
