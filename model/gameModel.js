d59c62403d984a1523b92bb0bcdc4969240a58b1
Parker Bjur <bjur.parker45@gmail.com>
2021-02-01 19:13:32 -0800

    Added gameModel

---
+++ model/gameModel.js
@@ -0,0 +1,24 @@
+mongoose = require('./dbConnection');
+
+const GameSchema = new mongoose.Schema({
+    running: Boolean,
+    rated: Boolean,
+    state: String,
+    moves: String,
+    events: [
+        {
+            turn: {
+                type: String, 
+                enum: ['BLACK', 'WHITE'],
+                default: 'BLACK'
+            },
+            wClock: Number,
+            bClock: Number,
+            move: String,
+        }
+    ]
+});
+
+const Game = mongoose.model('Game', GameSchema);
+
+exports.Game = Game
