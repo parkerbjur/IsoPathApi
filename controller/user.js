b0afa69b59e42ba44e3bfae55d9175f4af287698
Parker Bjur <bjur.parker45@gmail.com>
2021-02-02 15:18:21 -0800

    Add: User Authorize function

--- controller/user.js
+++ controller/user.js
@@ -70,3 +70,22 @@ exports.login = (req, res, next) => {
     },
   );
 };
+
+exports.authorize = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+    const { userId } = decodedToken;
+    if (req.body.userId !== userId) {
+      res.status(401).json({
+        error: new Error('Invalid User Id'),
+      });
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error('Invalid request!'),
+    });
+  }
+};
