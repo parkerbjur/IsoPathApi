b81d4508f118431046ef70a5756f573820aaf940
Parker Bjur <bjur.parker45@gmail.com>
2021-02-01 21:21:36 -0800

    Add: placeholder routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const user = require('../controller/controller');
+
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 3000;
 
 app.listen(port);
+
+router.post('/api/game/seek', (req, res) => {
+
+});
+
+router.post('/api/game/move', (req, res) => {
+
+});
+
+app.post('/api/challenge/', (req, res) => {
+
+});
+
+module.exports = router;
