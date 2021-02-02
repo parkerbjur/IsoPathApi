52f900d8822962386f97d1aa0023d2269a86c594
Parker Bjur <bjur.parker45@gmail.com>
2021-02-01 21:20:32 -0800

    Fix: eslint messages

--- model/userModel.js
+++ model/userModel.js
@@ -1,13 +1,13 @@
-mongoose = require('./dbConnection');
+const mongoose = require('./dbConnection');
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-    rating: Number,
-    games: [String],
+  username: String,
+  password: String,
+  email: String,
+  rating: Number,
+  games: [String],
 });
 
 const User = mongoose.model('User', UserSchema);
 
-exports.User = User
+exports.User = User;
