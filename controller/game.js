5801cf2ec8b3f03f249b80e309367ab270dfbbcd
Parker Bjur <bjur.parker45@gmail.com>
2021-02-04 15:03:35 -0800

    Add: accept challenfe and create game functions

--- controller/game.js
+++ controller/game.js
@@ -1,4 +1,5 @@
 const ChallengeModel = require('../model/challengeModel');
+const GameModel = require('../model/gameModel');
 
 exports.challenge = (req, res, next) => {
   const newChallenge = new ChallengeModel({
@@ -16,4 +17,40 @@ exports.challenge = (req, res, next) => {
       });
     },
   );
+};
+
+exports.acceptChallenge = (req, res, next) => {
+  const game = {
+    running: true,
+    rated: req.body.rated,
+  };
+
+  if (req.body.challengerColor === 'RANDOM') {
+    const players = [req.body.challenger, req.body.challengee];
+    const white = Math.floor(Math.random() * players.lenght);
+    game.white = players[white];
+    game.black = players[white + 1];
+  } else if (req.body.color === 'BLACK') {
+    game.white = req.body.challengee;
+    game.black = req.body.challenger;
+  } else if (req.body.color === 'WHITE') {
+    game.white = req.body.challenger;
+    game.black = req.body.challengee;
+  } else {
+    res.status(401).json({
+      message: 'Invalid Player Color',
+    });
+  }
+
+  game.save().then((data) => {
+    res.status(4001).json({
+      message: 'Game Created',
+      data,
+    });
+  });
+};
+
+exports.createGame = (game) => {
+  const newGame = new GameModel({ game });
+  newGame.save();
 };
