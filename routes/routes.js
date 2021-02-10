d491792e130bfe8c1c8c4a969a1aa731630e098a
Parker Bjur <bjur.parker45@gmail.com>
2021-02-09 19:59:20 -0800

    Change: express router configuration

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,15 @@
 require('dotenv');
 const express = require('express');
+const bodyParser = require('body-parser');
+const gameRoutes = require('./game');
+const userRoutes = require('./user');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/game', require('./game'));
-app.use('/user', require('./user'));
+app.use(bodyParser.json());
+app.use('/api/game', gameRoutes);
+app.use('/api/user', userRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
