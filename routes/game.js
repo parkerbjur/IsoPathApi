3c99ee2b477121880e31233575ad0cf9fec1086f
Parker Bjur <bjur.parker45@gmail.com>
2021-03-09 20:54:49 -0800

    Update: challenge path

--- routes/game.js
+++ routes/game.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { challenge } = require('../controller/game');
 const { authorize } = require('../controller/user');
 
-router.post('/api/challenge/', authorize, challenge);
+router.post('/challenge', authorize, challenge);
 
 module.exports = router;
