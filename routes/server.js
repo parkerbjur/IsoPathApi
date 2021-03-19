943b0e1ac988c49dd8b47d600090ba9130a4a998
Parker Bjur <38863599+parkerbjur@users.noreply.github.com>
2021-03-18 21:24:37 -0700

    Add: event registration for specific event types

--- routes/server.js
+++ routes/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const app = require('express')();
 const bodyParser = require('body-parser');
+const { JsonWebTokenError } = require('jsonwebtoken');
 const WebSocket = require('ws');
 const gameRoutes = require('./game');
 const userRoutes = require('./user');
@@ -13,10 +14,15 @@ app.use('/api/user', userRoutes);
 app.get('/', (req, res) => { res.json(req.body); });
 
 const wsServer = new WebSocket.Server({ noServer: true });
+
 wsServer.on('connection', (socket) => {
-  socket.on('message', (message) => console.log(message));
-  exports.socket = socket;
+  socket.on('message', (data) => {
+    wsServer.functions[JSON.parse(data.eventType)](data);
+  });
 });
+exports.wsServer.regesterListener = (data) => {
+  wsServer.functions[data.eventType] = data.function;
+};
 
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
