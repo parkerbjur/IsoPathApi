0a59af95dd41535ca996b246211b9ef54561ed89
Parker Bjur <38863599+parkerbjur@users.noreply.github.com>
2021-03-18 21:29:46 -0700

    Remove: JsonWebTokenError

--- routes/server.js
+++ routes/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const app = require('express')();
 const bodyParser = require('body-parser');
-const { JsonWebTokenError } = require('jsonwebtoken');
 const WebSocket = require('ws');
 const gameRoutes = require('./game');
 const userRoutes = require('./user');
