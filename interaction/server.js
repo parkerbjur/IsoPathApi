1ddabd3d819d25731222ed279b14385a56c5937d
Parker Bjur <38863599+parkerbjur@users.noreply.github.com>
2021-07-02 08:34:22 -0700

    Convert: server to use socket.io

--- interaction/server.js
+++ interaction/server.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const app = require('express')();
 const bodyParser = require('body-parser').json();
-const WebSocket = require('ws');
+const http = require('http');
+
+const server = http.createServer(app);
+const Server = require('socket.io');
+
+const io = new Server(server);
 const gameRoutes = require('./game');
 
 const port = process.env.PORT || 3000;
@@ -9,38 +14,3 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser);
 app.use('/api/game', gameRoutes);
 app.get('/', (req, res) => { res.json(req.body); });
-
-const wsServer = new WebSocket.Server({ noServer: true });
-
-wsServer.startup = [];
-wsServer.functions = {};
-wsServer.on('connection', (socket) => {
-  wsServer.startup.forEach((action) => {
-    action();
-  });
-
-  socket.on('message', (data) => {
-    wsServer.functions[JSON.parse(data).eventType](data);
-  });
-});
-
-wsServer.registerStatupListener = (data) => {
-  wsServer.startup.push(data);
-};
-wsServer.registerListener = (data) => {
-  wsServer.functions[data.eventType] = data.function;
-};
-
-const server = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-server.on('upgrade', (request, socket, head) => {
-  wsServer.handleUpgrade(request, socket, head, (ws) => {
-    wsServer.emit('connection', ws, request);
-  });
-});
-
-exports.registerListener = wsServer.registerListener;
-exports.registerStatupListener = wsServer.registerStatupListener;
-require('./listeners')();
