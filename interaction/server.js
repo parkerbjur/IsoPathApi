0aefe338b7c477c81b85c0dbf93de91d34247fc4
Parker Bjur <38863599+parkerbjur@users.noreply.github.com>
2021-07-02 10:22:50 -0700

    Clean: unused game object

--- interaction/server.js
+++ interaction/server.js
@@ -7,16 +7,20 @@ const server = http.createServer(app);
 const Server = require('socket.io');
 
 const io = new Server(server);
-const gameRoutes = require('./game');
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser);
-app.use('/api/game', gameRoutes);
 app.get('/', (req, res) => { res.json(req.body); });
 
 io.on('connection', (socket) => {
   console.log(`a user connected at ${socket}`);
+
+  
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(port, () => {
