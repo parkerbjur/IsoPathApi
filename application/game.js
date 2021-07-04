94f1951b5ee757c750cfb87077b7c34d65bc6d07
Parker Bjur <38863599+parkerbjur@users.noreply.github.com>
2021-07-04 14:14:28 -0700

    Start: framework for play move functionality

--- application/game.js
+++ application/game.js
@@ -1,7 +1,19 @@
+const server = require('../interaction/server');
+const { Board } = require('./board');
+
 module.exports = {
+  games: {},
+
   createGame: (socket1, socket2) => {
     const gameID = Date();
     socket1.join(gameID);
     socket2.join(gameID);
+    this.games[gameID] = new Board(socket1, socket2, gameID);
+    server.io.to(gameID).emit('game:create');
+  },
+
+  playMove: (args) => {
+    const { gameID } = JSON.parse(args);
+    this.games.gameID.playMove();
   },
 };
--- interaction/server.js
+++ interaction/server.js
@@ -7,6 +7,7 @@ const server = http.createServer(app);
 const Server = require('socket.io');
 
 const io = new Server(server);
+exports.io = io;
 
 const port = process.env.PORT || 3000;
 
